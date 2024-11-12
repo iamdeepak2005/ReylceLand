fde29775df25626bad745fb27a548001c6db5b02
iamdeepak2005 <166904926+iamdeepak2005@users.noreply.github.com>
2024-11-12 13:31:52 +0530

    routes added

--- ReylceLand/src/app/app.routes.ts
+++ ReylceLand/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConactComponent } from './conact/conact.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServiceComponent } from './service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'contact',component:ConactComponent},
+    {path:'about',component:AboutusComponent},
+    {path:'service',component:ServiceComponent},
+];
